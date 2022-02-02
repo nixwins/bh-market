5a2946d9c8116a53420553c54e53b1dbe1b8e86c
nixwins <halaulilau@gmail.com>
2022-02-02 19:08:35 +0600

    pagination with route path

---
+++ pages/products/page/_page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <products-pagination />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductPage',
+  components: {
+    ProductsPagination: () =>
+      import('@/components/app/product/ProductsPagination.vue'),
+  },
+}
+</script>
