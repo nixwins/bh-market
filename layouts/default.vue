60044162f3024fcad08e043733e2fdf9aa06037d
nixwins <halaulilau@gmail.com>
2022-02-02 19:09:39 +0600

    tabs sync with storage

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,92 +1,85 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
+  <div class="default-layout">
+    <v-app>
       <v-container>
+        <app-header />
         <Nuxt />
+        <v-snackbar v-model="snackbar.isShow">
+          {{ snackbar.text }}
+
+          <template v-slot:action="{ attrs }">
+            <v-btn
+              color="pink"
+              text
+              v-bind="attrs"
+              @click="setSnackbar({ isShow: false })"
+            >
+              Close
+            </v-btn>
+          </template>
+        </v-snackbar>
+
+        <v-dialog v-model="isShowCart" width="500">
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2">
+              Коризна
+            </v-card-title>
+
+            <v-card-text>
+              <shop-cart />
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="setIsShowCart()">
+                закрыть
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+    </v-app>
+  </div>
 </template>
 
 <script>
+import { mapState, mapActions, mapMutations } from 'vuex'
+import { actionTypes } from '@/store/index'
+import {
+  mutationTypes as cartMutationTypes,
+  actionTypes as cartActionTypes,
+} from '@/store/cart'
+
 export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'default',
+  components: {
+    AppHeader: () => import('@/components/common/AppHeader.vue'),
+    ShopCart: () => import('@/components/app/cart/ShopCart.vue'),
+  },
+  computed: {
+    ...mapState({
+      snackbar: (state) => state.snackbar,
+      isShowCart: (state) => state.cart.isShowCart,
+    }),
+  },
+  mounted() {
+    // eslint-disable-next-line nuxt/no-env-in-hooks
+    if (process.client) {
+      window.addEventListener('storage', () => {
+        const products = JSON.parse(localStorage.getItem('cart')).cart.products
+        console.log(products)
+
+        this.syncProducts(products)
+      })
     }
+  },
+  methods: {
+    ...mapActions({ setSnackbar: actionTypes.changeSnackbar }),
+    ...mapActions('cart', { syncProducts: cartActionTypes.restoreProducts }),
+    ...mapMutations('cart', { setIsShowCart: cartMutationTypes.setIsShowCart }),
   },
 }
 </script>
