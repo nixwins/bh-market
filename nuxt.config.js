8328f0877027001daa465690a4765d6c88c9697a
nixwins <halaulilau@gmail.com>
2022-02-02 19:11:14 +0600

    persistence vuex

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     titleTemplate: '%s - bh-market',
     title: 'bh-market',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ru',
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,12 +16,16 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+  server: {
+     host: "0.0.0.0",
+    port: 8000 
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{src:'~/plugins/persistedstate.js', ssr:false}],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -43,23 +47,18 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://back2.bh.market/api/front',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+        light: {
+          primary: colors.blue,
+         
         },
       },
     },
