c91323ba7ce4ad01e3849b26b4ecf86019606699
nixwins <halaulilau@gmail.com>
2022-02-02 19:07:29 +0600

    added app header

---
+++ components/app/cart/CartBadge.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-badge :content="count" color="success" dark overlap>
+    <v-btn @click="setIsShowCart()" class="mx-2" color="primary" fab>
+      <v-icon> mdi-cart </v-icon>
+    </v-btn>
+  </v-badge>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+import { mutationTypes } from '@/store/cart'
+
+export default {
+  name: 'CartBadge',
+  computed: {
+    ...mapGetters('cart', { count: 'totalCount' }),
+  },
+  mounted() {
+    console.log('Mounted', this.count)
+  },
+  methods: {
+    ...mapMutations('cart', { setIsShowCart: mutationTypes.setIsShowCart }),
+  },
+}
+</script>
---
+++ components/common/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header mb-12">
+    <v-row>
+      <v-col cols="10">
+        <h1><nuxt-link to="/">Тестовый интернет магазин</nuxt-link></h1>
+      </v-col>
+      <v-col cols="2">
+        <cart-button />
+      </v-col>
+    </v-row>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  components: {
+    CartButton: () => import('@/components/app/cart/CartBadge.vue'),
+  },
+}
+</script>
